8bf5aad5eca8a8bd9bc6b9f27dfde130e4af6cc3
Diego Lopez <die_elias2012@hotmail.com>
2023-08-31 16:26:57 -0600

    Tareaa 5 Econtrar el numero mas pequeño - Lopez Martinez Diego Elias

--- smaller/smaller.js
+++ smaller/smaller.js
@@ -1,1 +1,15 @@
-console.log('Hello world');
+function tarea5() {
+    let buscador = null;
+
+    const A = [4, 3, 5, 7, -4, 7, 4, 1, -9];
+
+    for (let i = 0; i < A.length; i++) {
+        if (buscador === null || A[i] < buscador) {
+            buscador = A[i];
+        }
+    }
+    
+    console.log("El número menor es: " + buscador);
+}
+
+tarea5();
