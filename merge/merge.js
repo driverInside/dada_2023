98439028d9fcabcf2af1d432e41e1f5abcb51297
Hiram E. Perez <hiram.eps@gmail.com>
2023-10-09 09:27:25 -0500

    incomplete solution

--- merge/merge.js
+++ merge/merge.js
@@ -1,15 +1,50 @@
 /**
- * Dados dos arreglos ordenados A, B; producir un nuevo arreglo C uniendo A y B
+ * Dados dos arreglos ordenados A, B; producir un nuevo arreglo C ordenado ascendentemente uniendo A y B 
  * 
- * A = [1, 2, 7, 9, 14, 23]
- * B = [3, 8, 9, 10, 16, 18, 19]
- * 
- * |
- * v
- * 
- * C = [1, 2, 3, 7, 8, 9, 9, 10, 14, 16, 18, 19, 23]
+ * A = [1, 5, 7, 9, 11, 12]       N = 5
+ *               i
+ * B = [3, 4, 9, 10]          M = 4
+ *                j
+ * C = [1, 3,  ]
+ *            k
+ *
+ *  Comparar los dos elementos más pequeños.
+ *  El más pequeño, lo voy a sacar de su arreglo para ingresarlo al nuevo
+ *       
+ *       [1, 3, 4, 5, 7, 9, 9, 10, 11]
  * 
  * Merge(A, B)
  * // input: dos arreglos A y B ordenados
  * // output: la unión de A y B ordenada
  */
+
+function merge(arrayA, arrayB) {
+  let i = 0;
+  let j = 0;
+  let k = 0;
+  const result = [];
+
+  while (result.length < arrayA.length + arrayB.length) {
+    if (arrayA[i] <= arrayB[j]) {
+      result[k] = arrayA[i];
+
+      if (i < arrayA.length) {
+        i++;
+      }
+    } else {
+      result[k] = arrayB[j];
+
+      if (j < arrayB.length) {
+        j++;
+      }
+    }
+    k++;
+  }
+
+  return result;
+}
+
+const arrA = [1, 5, 7, 9, 11, 12] ;
+const arrB = [3, 4, 9, 10];
+
+console.log(merge(arrA, arrB)); // 
